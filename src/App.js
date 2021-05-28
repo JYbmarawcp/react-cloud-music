e55361bfb7b92c3e547c37e5930f01bc95224536
傅佳杭 <jhfu1@leqee.com>
2021-05-28 14:05:28 +0800

    完成项目基础搭建

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux'
+import { GlobalStyle } from './style'
+import { IconStyle } from './assets/iconfont/iconfont'
+import { renderRoutes } from 'react-router-config'
+import store from './store'
+import routes from './routes/index'
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
